a9ad38df6c547ddcfbc021b9fc28cb13630b7b1f
shagors <shajjadshagor@gmail.com>
2022-04-16 23:45:03 +0600

    react-router, bootstrap, react firebase hook install

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <h1>This is home page</h1>
     </div>
   );
 }
