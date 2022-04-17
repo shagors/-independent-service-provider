12c154275fe689d1f4e96f827ebeffe4a892cd29
shagors <shajjadshagor@gmail.com>
2022-04-17 22:20:12 +0600

    reposive reviews syster and footer color fix

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -4,8 +4,8 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div className='footer mt-3'>
-            <p className='text-center pt-3'>copyright &copy; 2022</p>
-            
+            <p className='text-center pt-3 text-white'>copyright &copy; 2022</p>
+
         </div>
     );
 };
