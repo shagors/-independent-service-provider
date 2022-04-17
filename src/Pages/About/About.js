762a1335d329a2c194a8a215377e20ac2bd258f5
shagors <shajjadshagor@gmail.com>
2022-04-17 22:10:24 +0600

    about me page and footer page add and design

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import './About.css'
+import shajjad from '../../images/shajjad.JPG'
 
 const About = () => {
     return (
-        <div>
-            <h1>This is about page</h1>
+        <div className='container about-page'>
+            <div className='row d-flex'>
+                <div className='col-lg-4 about-left'>
+                    <img src={shajjad} alt="" />
+                </div>
+                <div className='col-lg-8 text-center about-right'>
+                    <h2>I am <span className='text-primary nme'>Shajjad Hossan</span></h2>
+                    <p className=''>In next few months i want to be a front-end and back-end developer. In This year make me a a programmer. If I want achieve this goal I should work hard and full effort from me. And also follow P-Hero teams instructions. If I work hard in sha Allah I achieve this goal</p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './Footer.css'
 
 const Footer = () => {
     return (
-        <div>
-            <h1>This is Footer</h1>
+        <div className='footer mt-3'>
+            <p className='text-center pt-3'>copyright &copy; 2022</p>
+            
         </div>
     );
 };
